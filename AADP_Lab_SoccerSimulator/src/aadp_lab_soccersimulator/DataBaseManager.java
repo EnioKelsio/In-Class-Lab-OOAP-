ec0f3dccbef80cbcf3ce708c68b6ede1f31d0523
Enio Kelsio <100359737+EnioKelsio@users.noreply.github.com>
2023-12-15 14:21:09 +0000

    new commit

--- AADP_Lab_SoccerSimulator/src/aadp_lab_soccersimulator/DataBaseManager.java
+++ AADP_Lab_SoccerSimulator/src/aadp_lab_soccersimulator/DataBaseManager.java
@@ -27,28 +27,31 @@ public class DataBaseManager {
       private Connection connct;
     
     // This is the constructor for the class
-    public DataBaseManager(String dbName, String[] teams, String DB_URL, String USER, String PASS) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public DataBaseManager(String dbName, String[] teams, String DB_URL, String USER, String PASS) 
+            
+            // This method sets up the database       
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        
         this.dbName = dbName;
         this.teams = teams;
         this.DB_URL = DB_URL;
         this.USER = USER;
         this.PASS = PASS; {
-        // Implement the constructor
+
     }
     
-    // This method sets up the database
-
-  
-  
         // Load the MySQL driver
         Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        
         try {
             // Establish a connection to the database
             Connection connct = DriverManager.getConnection("jdbc:mysql://localhost/", USER, PASS);
             Statement stmt = connct.createStatement();
+            
             // Create the schema if it doesn't exist
             stmt.execute("CREATE SCHEMA IF NOT EXISTS " + dbName +";");
             stmt.execute("USE " + dbName + ";");
+            
             // Create a table for each team
             for (String team : teams) {
                 stmt.execute(
--- AADP_Lab_SoccerSimulator/src/aadp_lab_soccersimulator/SoccerSimulator.java
+++ AADP_Lab_SoccerSimulator/src/aadp_lab_soccersimulator/SoccerSimulator.java
@@ -9,8 +9,12 @@ import java.util.Scanner;
 /**
  *
  * @author Helder Camara
-// This class runs the soccer simulation
+ * */
+
+ //This class runs the soccer simulation
+
 public class SoccerSimulator {
+    
     // These are the instance variables for the class
     private int option;
     private boolean exit;
@@ -23,20 +27,25 @@ public class SoccerSimulator {
 
     // This method runs the soccer simulation
     public void runSimulator() {
+        
         do {
             // Print the menu options
             System.out.println("Welcome! What would you like to do today? Please enter the number corresponding to your choice.");
             System.out.println("1. Enter a new player to a team.");
             System.out.println("2. See the players on a team.");
             System.out.println("3. Simulate a number of matches.");
-            System.out.println("4. Exit the programme.");            
+            System.out.println("4. Exit the programme.");  
+            
             try {
                 // Get the user's choice
                 option = Integer.parseInt(sc.nextLine());
+                
                 if (option == 1) {
                     // Add your code here
                 }
+                
             } catch (NumberFormatException e) {
+                
                 // Print an error message for invalid input
                 System.out.println("Invalid input. Please try again.");
             }
